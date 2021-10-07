6f8d061050457b4227eacbe1443ebc5051a8ff58
luysla <dyana.tavares1@gmail.com>
2021-10-06 21:58:36 -0300

    Adding initial page desktop

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import './style.scss';
+
+import iconQuestion from '../../assets/img/icon-question.png';
+import imgPokemon from '../../assets/img/image-pokemon.png';
 
 const Home = () => {
     return (
-        <p>Página Home</p>
+        <div className="main">
+			<p className="title">Quem é esse</p>
+			<img src={imgPokemon} alt="" />
+			<img src={iconQuestion} alt="" />
+
+			<button className="bt-start" onClick={() => console.log('teste')}>
+				Começar
+			</button>
+		</div>
     )
 }
 
